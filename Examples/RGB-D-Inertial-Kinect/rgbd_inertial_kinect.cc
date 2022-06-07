0947bbf341e2bd3577cd28a098eac285b1158b4b
Valentin Tsatskin <val@valtsatskin.com>
2022-06-07 12:37:15 +0100

    init RGB-D-Inertial-Kinect

---
+++ Examples/RGB-D-Inertial-Kinect/rgbd_inertial_kinect.cc
@@ -0,0 +1,40 @@
+/**
+ * This file is part of ORB-SLAM3
+ *
+ * Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+ * Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+ *
+ * ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with ORB-SLAM3.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <signal.h>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <chrono>
+#include <ctime>
+#include <sstream>
+
+#include <condition_variable>
+
+#include <opencv2/core/core.hpp>
+
+#include <System.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
